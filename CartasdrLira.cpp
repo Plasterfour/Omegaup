3a09cbae5538e073613e36a7e0eb889683041a12
Francisco Gallegos Portillo <42197092+Plasterfour@users.noreply.github.com>
2018-09-09 21:32:10 -0500

    Add files via upload

---
+++ CartasdrLira.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, const char * argv[]) {
+	int a, i, sum=0;
+	scanf("%d", &a);
+	char N[a];
+	scanf("%s", &N[0]);
+	for (i=1; i<=a/2; i++){
+		if (N[i]==N[i+1])
+			sum+=1;
+	}
+	printf("%d", sum);
+	return 0;
+}
