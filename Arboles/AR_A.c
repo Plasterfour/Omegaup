805c0713b440e0f3b598813b044e1094b5d1ac48
Francisco Gallegos Portillo <42197092+Plasterfour@users.noreply.github.com>
2018-10-15 00:01:44 -0500

    Add files via upload

---
+++ Arboles/AR_A.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+typedef struct Arbol Arbol;
+struct Arbol{
+	int dato;
+	struct Arbol * izq;
+	struct Arbol * derecha;
+};
+
+Arbol * NuevoNodo(int dato);
+void insertarArbolrecursivo(Arbol ** r, int dato, int niveles);
+int main(int argc, char **argv){
+	int datos, nodo, i, nivel=0;
+	Arbol * r=NULL;
+	scanf("%d", &datos);
+	for (i=1; i<=datos; i++){
+		scanf("%d", &nodo);
+		insertarArbolrecursivo(&r, nodo, nivel);
+	}
+}
+Arbol * NuevoNodo (int dato){
+	Arbol * nuevo = (Arbol*)malloc(sizeof(Arbol));
+	nuevo->dato=dato;
+	nuevo->derecha=NULL;
+	nuevo->izq=NULL;
+	return nuevo;
+}
+void insertarArbolrecursivo(Arbol ** r, int dato, int niveles){
+	if(*r==NULL){
+		printf("%d\n", niveles);
+		Arbol * nuevo = NuevoNodo(dato);
+		*r = nuevo;
+	}
+	else{
+		Arbol * aux = *r;
+		if(dato>(aux->dato)){
+			niveles++;
+			insertarArbolrecursivo(&aux->derecha, dato, niveles);
+		}
+		else {
+			niveles++;
+			insertarArbolrecursivo(&aux->izq, dato, niveles);
+		}
+	}
+}
---
+++ Arboles/AR_C.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
+typedef struct Arbol Arbol;
+struct Arbol{
+	int dato;
+	struct Arbol * izq;
+	struct Arbol * derecha;
+};
+
+Arbol * NuevoNodo(int dato);
+void insertarArbolrecursivo(Arbol ** r, int dato);
+void preOrden(Arbol * r);
+int main(int argc, char **argv){
+	int datos, nodo, i;
+	Arbol * r=NULL;
+	scanf("%d", &datos);
+	int arrayaux[datos];
+	for (i=1; i<=datos; i++){
+		scanf("%d", &nodo);
+		arrayaux[i]=nodo;
+	}
+	for (i=datos; i>0; i--){
+		insertarArbolrecursivo(&r, arrayaux[i]);
+	}
+	preOrden(r);
+}
+Arbol * NuevoNodo (int dato){
+	Arbol * nuevo = (Arbol*)malloc(sizeof(Arbol));
+	nuevo->dato=dato;
+	nuevo->derecha=NULL;
+	nuevo->izq=NULL;
+	return nuevo;
+}
+void insertarArbolrecursivo(Arbol ** r, int dato){
+	if(*r==NULL){
+		Arbol * nuevo = NuevoNodo(dato);
+		*r = nuevo;
+	}
+	else{
+		Arbol * aux = *r;
+		if(dato>(aux->dato)){
+			insertarArbolrecursivo(&aux->derecha, dato);
+		}
+		else {
+			insertarArbolrecursivo(&aux->izq, dato);
+		}
+	}
+}
+void preOrden(Arbol * r){
+	if(r!=NULL){
+		printf("%d ", r->dato);
+		preOrden(r->izq);
+		preOrden(r->derecha);
+	}
+}
