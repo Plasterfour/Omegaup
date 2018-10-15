ea109a67eb68bd2b276753e02959ba386e76fcd9
Francisco Gallegos Portillo <42197092+Plasterfour@users.noreply.github.com>
2018-10-15 00:02:12 -0500

    Add files via upload

---
+++ Recursividad/RE_A.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int factorial(int n);
+int main(int argc, char *argv[]) {
+	int n;
+	scanf("%d",&n);
+	//factorial(n);
+	printf("%d",factorial(n));
+	return 0;
+}
+
+int factorial(int n)
+{
+	if(n<5)
+	{
+		return 1;
+	}else
+	{
+		return factorial(n-2)*5;
+	}
+}
---
+++ Recursividad/RE_C.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int mcd();
+int main(int argc, char *argv[]) {
+	int a,b;
+	scanf("%d %d",&a,&b);
+	int res=mcd(a,b);
+	printf("%d",res);
+	return 0;
+}
+int mcd(int a,int b){
+	int res=1;
+	for(int i=1;i<a;i++)
+	{
+		if((a%i==0)&&(b%i==0))
+		{
+			res=i;
+		}
+	}
+	return res;
+}
